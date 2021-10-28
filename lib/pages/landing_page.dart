2f9e3ec6c7d625e118090c4fef882a6c83a2dd77
Varun <varungupta2015135@gmail.com>
2021-10-28 20:11:02 +0530

    Completed landing page

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(42, 42, 42, 1),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: queryData.size.width / 10,
+              vertical: queryData.size.height / 10,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Set your Financial Goals',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const Text(
+                  'Track your expenses',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Text(
+                        "Get Started",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                      Icon(Icons.arrow_right_alt, color: Colors.white),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            child: Image.asset("assets/images/landing.png"),
+            bottom: 0,
+            right: 0,
+          )
+        ],
+      ),
+    );
+  }
+}
